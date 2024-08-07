5f0a101991898ed82dce3542ab30f2dd9bc4ebe0
Leon Schroeder <user@Leons-MacBook-Air.fritz.box>
2024-08-08 01:03:50 +0200

    init

---
+++ README.md
@@ -0,0 +1,49 @@
+# ch32v307-no-sdk
+
+![License](https://img.shields.io/badge/license-MIT-blue.svg) ![Version](https://img.shields.io/badge/version-1.0.0-green.svg)
+
+## Description
+
+This is a minimal build-environment to programm the ch32v3007.
+Just create a new folder for you project copy the minimal Makefile and done.
+
+Makefile:
+```make
+all: flash
+
+TARGET:={path to your project Folder}
+
+include ../../ch32v307.mk
+
+flash: cv_flash
+clean: cv_clean
+```
+
+## Installation
+
+```bash
+# Clone the repository
+git clone https://github.com/x0sig/ch32v307-no-sdk
+
+# Navigate into the directory
+cd ch32v307-no-sdk
+
+# Install dependencies
+
+# it uses the riscv-compiler:
+https://github.com/xpack-dev-tools/riscv-none-elf-gcc-xpack
+# and the flasher tool:
+https://github.com/ch32-rs/wlink
+
+# Run the application
+make
+make clean
+# Running make also flashes the project automaticaly
+```
+
+## References
+
+this project is based on:
+`https://github.com/openwch/ch32v307`
+and inspired by:
+`https://github.com/cnlohr/ch32v003fun`
---
+++ ch32v307.mk
@@ -0,0 +1,40 @@
+CC = riscv-none-elf-gcc
+
+CURRENT_DIR := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
+SRC_DIR := $(CURRENT_DIR)src
+OBJ_DIR := $(CURRENT_DIR)obj
+INC_DIR := $(CURRENT_DIR)inc
+
+# Header files
+LIBS += -I$(INC_DIR)
+# Source files
+SRCS := $(wildcard $(SRC_DIR)/*.c) $(wildcard $(SRC_DIR)/*.S)
+# Object files
+OBJS := $(patsubst $(SRC_DIR)/%.c,$(OBJ_DIR)/%.o,$(SRCS))
+OBJS := $(patsubst $(SRC_DIR)/%.S,$(OBJ_DIR)/%.o,$(OBJS))
+# Dependency files
+DEPS := $(OBJS:.o=.d)
+
+# Rule to compile .S files to .o files
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.S
+	$(CC) -march=rv32imac_zicsr -mabi=ilp32 -msmall-data-limit=8 -msave-restore -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-common -Wunused -Wuninitialized  -g -x assembler-with-cpp $(LIBS) -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+
+# Rule to compile .c files to .o files
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
+	$(CC) -march=rv32imac_zicsr -mabi=ilp32 -msmall-data-limit=8 -msave-restore -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-common -Wunused -Wuninitialized  -g $(LIBS) -std=gnu99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+
+main.o: main.c
+	$(CC) -march=rv32imac_zicsr -mabi=ilp32 -msmall-data-limit=8 -msave-restore -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-common -Wunused -Wuninitialized  -g $(LIBS) -std=gnu99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+
+# Rule to compile main.c project
+main.elf: $(OBJS) main.o
+	$(CC) -march=rv32imac_zicsr -mabi=ilp32 -msmall-data-limit=8 -msave-restore -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-common -Wunused -Wuninitialized  -g -T link.ld -nostartfiles -Xlinker --gc-sections -L"$(CURRENT_DIR)" --specs=nano.specs --specs=nosys.specs -o "$@" main.o $(OBJS) $(LIBS)
+
+cv_clean:
+	rm -f $(OBJ_DIR)/*
+	rm -f main.elf main.d main.o
+
+cv_flash: main.elf
+	@wlink flash main.elf
+
+.PHONY: cv_clean cv_flash
